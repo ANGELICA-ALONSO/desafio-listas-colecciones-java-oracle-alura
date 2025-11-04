9f93a36e75bf10d1ce7021fdd09cfc8fc1337229
ANGELICA ALONSO ORTIZ <130294701+ANGELICA-DATA-SCIENCE@users.noreply.github.com>
2025-11-04 12:54:56 -0600

    Se agregó la clase Compra y modificaciones en TarjetaCredito

---
+++ src/Compra.java
@@ -0,0 +1,24 @@
+public class Compra {
+    private double valor;
+    private String descripcion;
+
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: " + "valor=" + valor +
+                ", descripcion='" + descripcion;
+    }
+}
